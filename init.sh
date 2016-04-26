af403a1101df3384c761e046ab32f6a992129b35
newepr <13909719056@163.com>
2016-04-26 23:39:14 +0800

    Changs init_centos.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,81 @@
 #!/bin/bash
 #
 #
+
 function set_hostname {
 
 }
+
+function set_localtime {
+
+}
+
+function set_ipaddr {
+
+}
+
+function set_dns {
+
+}
+
+function set_yum {
+
+}
+
+function set_profile {
+
+}
+
+function set_unlimit {
+
+}
+
+function set_sysctl {
+
+}
+
+function set_tty {
+
+}
+
+function set_inittab {
+
+}
+
+function set_selinux {
+
+}
+
+function set_iptables {
+
+}
+
+function set_ssh {
+
+}
+
+function set_service {
+
+}
+
+function set_ipv6 {
+
+}
+
+function set_locales {
+
+}
+
+function set_dpkg {
+
+}
+
+function set_rsyslog {
+
+}
+
+function set_logrotate {
+
+}
+
+
