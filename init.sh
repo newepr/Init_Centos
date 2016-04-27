6e434d63d95dc74916d5f16d851e7aec93448122
newepr <13909719056@163.com>
2016-04-27 23:54:08 +0800

    change init.sh for 2016-04-27

--- init.sh
+++ init.sh
@@ -15,82 +15,101 @@ function wget_install(){
 }
 
 function hostname() {
+	msg gr Hostname
 	echo -n "Please Enter Hostname:"
 	read -e OPTION
 	[ -f "/etc/sysconfig/network" ] && `sed -i 's/HOSTNAME=*/HOSTNAME='"$OPTION"'/g' /etc/sysconfig/network`
+	["`grep -w 'HOSTNAME' /etc/sysconfig/network`" == "$OPTION" ] && msg || hostname
 }
 
 function localtime() {
-
+	msg gr "Time Zone"
+	
 }
 
 function ipaddr() {
+	msg gr "Ip Address"
 
 }
 
 function dns() {
+	msg gr "DNS server"
 
 }
 
 function yum_repo() {
+	msg gr "Yum Repo"
 
 }
 
 function profile() {
+	msg gr "Profile"
 
 }
 
 function unlimit() {
+	msg gr "Ulimit"
 
 }
 
 function sysctl() {
+	msg gr "sysctl.conf"
 
 }
 
 function getty() {
+	msg gr "Getty"
 	echo -e "\033[32m >>> Configuring: Gettys ... \033[0m"
-    # Remove Unneeded Getty Instances
-    sed -e 's/\(^[2-6].*getty.*\)/#\1/' -i /etc/inittab
+	sed -e 's/\(^[2-6].*getty.*\)/#\1/' -i /etc/inittab
 }
 
 function inittab() {
+	msg gr "Control+alt+Delete"
 
 }
 
 function selinux() {
+	msg gr "Selinux"
 
 }
 
 function iptables() {
+	msg gr "Iptables"
 
 }
 
 function ssh() {
+	msg gr "Openssh-server"
 
 }
 
 function service() {
+	msg gr "Service"
 
 }
 
 function ipv6() {
+	msg gr "Disable Ipv6"
 
 }
 
 function locales() {
+	msg gr "Locales"
 
 }
 
 function dpkg() {
+	msg gr "Bases Dpkg"
 
 }
 
 function rsyslog() {
+	msg gr "Rsyslog"
 
 }
 
 function logrotate() {
+	msg gr "Logrotate"
 
 }
 
